e0a9c0dcc452705dd1bb6390eecd73ed6f6a8173
xmita-ghx <ghoraixmita1@gmail.com>
2026-07-13 01:22:16 +0530

    fix: remove incorrect .py extensions from package imports

--- marine_routing/__init__.py
+++ marine_routing/__init__.py
@@ -5,7 +5,7 @@ A Qiskit-based optimization package utilizing QAOA to solve the
 Asymmetric Traveling Salesperson Problem (ATSP) for autonomous marine drone fleets.
 """
 
-from .cost_models.py import *
-from .quantum_engine.py import *
+from .cost_models import *
+from .quantum_engine import *
 
 __version__ = "1.0.0"
